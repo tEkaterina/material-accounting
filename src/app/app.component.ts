91c279050aa12dfefeca7ad796685867b4c21664
tEkaterina <ekaterina.tselishcheva@gmail.com>
2017-09-13 22:57:38 +0300

    Create menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu',
+  template: '',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+}
