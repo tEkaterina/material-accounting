2d3a7a160f756d091ad8741e45974fcbc92853a6
tEkaterina <ekaterina.tselishcheva@gmail.com>
2017-09-14 20:42:04 +0300

    Integrate with Angular Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //material design components
+    BrowserAnimationsModule, MdButtonModule, MdCheckboxModule, MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'menu',
-  template: '',
+  templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
